cf9a838d8ef2089cc40b6afc6c1ef2e87c88f441
Carlos Encine <contato@carlosencine.com>
2021-10-17 02:52:01 -0300

    Add decorators for documentation

--- src/Carlosencine.Api/Controllers/V1/HelloController.cs
+++ src/Carlosencine.Api/Controllers/V1/HelloController.cs
@@ -2,9 +2,7 @@
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net.Mime;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -14,19 +12,42 @@ namespace Carlosencine.Api.Controllers.V1
     [ApiController]
     public class HelloController : ControllerBase
     {
+        #region Properties
+        
         public IMediator Mediator { get; }
 
+        #endregion
+
+        #region Constructor
+        
         public HelloController(IMediator mediator)
         {
             Mediator = mediator;
         }
 
+        #endregion
+
+
+        #region Actions
+        
+        /// <summary>
+        /// Show asingle greeting to the user
+        /// </summary>
+        /// <param name="request">The name of the user</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns>the greeting to the user</returns>
+        [Produces(MediaTypeNames.Application.Json)]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(typeof(HelloResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [HttpPost]
         public async Task<ActionResult> GetHello([FromBody] HelloRequest request, CancellationToken cancellationToken)
         {
             var response = await Mediator.Send(request, cancellationToken);
 
             return Ok(response);
-        }
+        } 
+        
+        #endregion
     }
 }
