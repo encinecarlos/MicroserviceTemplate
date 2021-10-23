4a45e58afc8bca79c5b909e7e8cce0dc9e5a0584
Carlos Encine <contato@carlosencine.com>
2021-10-23 13:58:33 -0300

    Add some modifications

--- src/Carlosencine.Application/Queries/Hello/HelloQuery.cs
+++ src/Carlosencine.Application/Queries/Hello/HelloQuery.cs
@@ -1,14 +1,20 @@
 ﻿using Carlosencine.Application.Queries.Hello.Request;
+using Flunt.Notifications;
 using MediatR;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Carlosencine.Application.Queries.Hello
 {
-    public class HelloQuery : IRequestHandler<HelloRequest, HelloResponse>
+    public class HelloQuery : Notifiable<Notification>, IRequestHandler<HelloRequest, HelloResponse>
     {
         public Task<HelloResponse> Handle(HelloRequest request, CancellationToken cancellationToken)
         {
+            if (string.IsNullOrEmpty(request.Name))
+            {
+                AddNotification("Validation", "Name cannot be Null.");
+            }
+
             var response = new HelloResponse
             {
                 Message = $"Hello {request.Name}!"
